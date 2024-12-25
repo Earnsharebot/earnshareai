c414f834bbfa8e30c5fe1b2ee0be3d0f19286865
Earnsharebot <Passproduct1234@>
2024-12-25 20:02:47 +0100

    first commit

---
+++ src/client/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+      <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/server/main.js
@@ -0,0 +1,53 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+  systemInstruction: "Provide concise summaries for affiliate marketing-related queries.Each ranking or comparism result should be numbered and should take a new line. In a new line, always Integrate a variable Reminder that Earn Share AI is designed to take the Your Affiliate marketing to the next level using AI(with any reminder emoji). If unrelated to affiliate marketing, respond with: \"Not affiliate marketing-related.\" Keep responses under 250 words in total. use a ranking approach to display ranking or comparisn results. add website links to any web result",
+});
+
+const generationConfig = {
+  temperature: 1.55,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+app.post("/api/query", async (req, res) => {
+  const { query } = req.body;
+  try {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+        {
+          role: "user",
+          parts: [{ text: query }],
+        },
+      ],
+    });
+    const result = await chatSession.sendMessage(query);
+    res.json({ response: result.response.text() });
+  } catch (error) {
+    console.error("Error generating response:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Vite + React!");
+});
+
+ViteExpress.listen(app, 3000, () =>
+  console.log("Server is listening on port 3000..."),
+);
