bcb0534b8081bd20e4a182908f4433665c3552f2
Earnsharebot <Passproduct1234@>
2024-12-26 14:59:37 +0100

    connected mongodb to express backend

--- src/server/main.js
+++ src/server/main.js
@@ -1,5 +1,6 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
@@ -7,6 +8,15 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log("Error: ", + err);
+});
+
+
 
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
