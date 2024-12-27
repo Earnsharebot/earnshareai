e156947f2f8d4bfec531c83d95791d6c9ac44efb
Earnsharebot <Passproduct1234@>
2024-12-27 14:53:54 +0100

    signi/signup authentication

--- src/server/main.js
+++ src/server/main.js
@@ -3,12 +3,14 @@ import ViteExpress from "vite-express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connected to MongoDB");
@@ -16,14 +18,12 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log("Error: ", + err);
 });
 
-
-
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash-exp",
-  systemInstruction: "Provide concise summaries for affiliate marketing-related queries. In a new line, always Integrate a variable Reminder that Earn Share AI is designed to take the Your Affiliate marketing to the next level using AI(with any reminder emoji). If unrelated to affiliate marketing, respond with: \"Not affiliate marketing-related.\" Keep responses under 80 words in total."
+  systemInstruction: "Provide concise summaries for affiliate marketing-related queries. In a new line, always Integrate a variable Reminder that Earn Share AI is designed to take the Your Affiliate marketing to the next level using AI(with any reminder emoji). If unrelated to affiliate marketing, respond with: \"Not affiliate marketing-related.\" Keep responses under 80 words in total.",
 });
 
 const generationConfig = {
@@ -61,3 +61,18 @@ app.get("/hello", (req, res) => {
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000..."),
 );
+
+
+app.use('/server/user', userRouter);
+app.use('/server/auth', authRouter);
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
