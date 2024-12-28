b99c45a69926e5011dcbadf895da5dddbbc5f1c9
Earnsharebot <Passproduct1234@>
2024-12-28 16:39:02 +0100

    google auth  + user-profile update

--- src/client/pages/SignUp.jsx
+++ src/client/pages/SignUp.jsx
@@ -1,8 +1,7 @@
 import { set } from "mongoose";
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
 
@@ -79,7 +78,9 @@ const SignUp = () => {
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
           {loading ? "Loading..." : "Sign Up"}
         </button>
+        <OAuth />
       </form>
+    
 
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
