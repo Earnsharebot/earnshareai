f29063901d87a2b74c57330366c35b8e06598c9e
Earnsharebot <Passproduct1234@>
2024-12-28 05:43:03 +0100

    changes

--- src/client/pages/SignIn.jsx
+++ src/client/pages/SignIn.jsx
@@ -8,81 +8,79 @@ import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSli
 
 const SignIn = () => {
 
-    const dispatch = useDispatch();
+    const [formData, setFormData] = useState({});  
     const { loading, error } = useSelector((state) => state.user);
-
     const navigate = useNavigate();
-    const [formData, setFormData] = useState({});
-
-
+    
+    const dispatch = useDispatch();
+  
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
-        console.log(formData);
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+      console.log(formData);
     };
-
+  
     const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            dispatch(signInStart());
-            const res = await fetch("/server/auth/signin", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            console.log(data);
-
-            if (data.success == false) {
-                dispatch(signInFailure(data.message));
-                return;
-            }
-
-            dispatch(signInSuccess(data));
-            navigate("/");
-
-        } catch (error) {
-            dispatch(signInFailure(error.message));
+      e.preventDefault();
+  
+      try {
+        dispatch(signInStart());
+        const res = await fetch("/api/auth/signin", {  
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        console.log(data);
+  
+        if (data.success == false) {
+          dispatch(signInFailure(data.message));
+          return;
         }
-
-
+  
+        dispatch(signInSuccess(data));
+        navigate("/");
+        
+      } catch (error) {
+        dispatch(signInFailure(error.message));
+      }
+  
     };
-
-
+  
     return (
-        <div className="w-full p-3 max-w-lg mx-auto">
-            <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-8 h-full">
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="border p-3 rounded-lg my-3"
-                    id="email"
-                    onChange={handleChange}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    className="border p-3 rounded-lg my-3"
-                    id="password"
-                    onChange={handleChange}
-                />
-                <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
-                    {loading ? "Loading..." : "Sign In"}
-                </button>
-            </form>
-
-            <div className="flex gap-2 mt-5">
-                <p>Dont have an account?</p>
-                <Link to={"/sign-up"}>
-                    <span className="text-blue-700 hover">Sign Up</span>
-                </Link>
-            </div>
-            {error && <p className="text-red-500 m">{error}</p>}
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="border p-3 rounded-lg"
+            id="email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border p-3 rounded-lg"
+            id="password"
+            onChange={handleChange}        
+          />
+          <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+          <OAuth />
+        </form>
+  
+        <div className="flex gap-2 mt-5">
+          <p>Dont have an account?</p>
+          <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
+          </Link>
         </div>
+        {error && <p className="text-red-500 m">{error}</p>}
+      </div>
     );
-};
-
-export default SignIn;
+  };
+  
+  export default SignIn;
